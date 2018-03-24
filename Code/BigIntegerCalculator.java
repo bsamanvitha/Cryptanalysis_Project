821cf70d814dc5cee3065c868aea550c6c01d952
Samanvitha Basole <samanvithab@users.noreply.github.com>
2018-03-23 20:24:27 -0700

    Update BigIntegerCalculator.java

--- Code/BigIntegerCalculator.java
+++ Code/BigIntegerCalculator.java
@@ -8,6 +8,6 @@ public class BigIntegerCalculator {
         BigInteger C = new BigInteger("455254144570149659309053721142936464401700360179699485409768697057718862"); 
         BigInteger d = new BigInteger("298082873119252710460625055681222869281660460407961156878212145977719009"); 
         BigInteger N = new BigInteger("501281908486219621910086584233925309600136539640088201223414043112175611");
-        System.out.println("Power : " + C.modPow(d, N));
+        System.out.println("M = " + C.modPow(d, N));
     }
 }
